935e2988111ae1fca893b1d7a671a5beefd9e97e
Colin Leung <colin@digi3studio.com>
2019-05-28 23:37:26 +0800

    update index.js file

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-module.exports = {
-  RouteList : require('./classes/RouteList'),
-  HelperDB : require('./classes/helper/HelperDB'),
-  HelperRoute : require('./classes/helper/HelperRoute'),
-};
-
 const K8 = require('k8mvc');
-K8.nodePackages[require.resolve('./index').replace('/index.js', '')] = true;
+K8.addNodeModules(require.resolve('./'));
